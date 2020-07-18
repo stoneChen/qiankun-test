50df0d087fbbecb5ec93aef5d84042e7296f275c
陈师东 <chensd@servyou.com.cn>
2020-07-18 15:28:52 +0800

    update micro app

--- micro-app/src/App.js
+++ micro-app/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+// 这行代码导致qiankun无法找到子应用正确添加的最后一个window下变量
+// 虽然可以通过【设置主应用中加载子应用的name与子应用的webpack output.library 值一致】来解决问题，但是增加了少量性能折损以及主子应用的耦合度
+window.console = window.console || (() => {});
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>micro app</h1>
     </div>
   );
 }
